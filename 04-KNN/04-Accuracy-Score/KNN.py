d48e6588f3ebe9402eef2ad579a5d94dcfd98ded
rannrann <239124260@qq.com>
2021-12-01 16:46:29 +0800

    04knn-04accuracy_score is done

--- 04-KNN/04-Accuracy-Score/KNN.py
+++ 04-KNN/04-Accuracy-Score/KNN.py
@@ -1,17 +1,18 @@
 import numpy as np
 from math import sqrt
 from collections import Counter
+from metrics import accuracy_score
 
-class KNNclassifier:
+class KNNClassifier:
     def __init__(self, k):
-        '''初始化kNN分类器'''
+        """初始化kNN分类器"""
         assert k >= 1, "k must be valid"
         self.k = k
         self._X_train = None
         self._y_train = None
     
     def fit(self, X_train, y_train):
-        '''根据训练数据集X_train和y_train训练kNN分类器'''
+        """根据训练数据集X_train和y_train训练kNN分类器"""
         assert X_train.shape[0] == y_train.shape[0], \
             "the size of X_train must be equal to the size of y_train"
         assert self.k <= X_train.shape[0], \
@@ -22,7 +23,7 @@ class KNNclassifier:
         return self
 
     def predict(self, X_predict):
-        '''给定待预测数据集X_predict，返回表示X_predict的结果向量'''
+        """给定待预测数据集X_predict，返回表示X_predict的结果向量"""
         assert self._X_train is not None and self._y_train is not None, \
             "must fit before predict!"
         assert X_predict.shape[1] == self._X_train.shape[1], \
@@ -31,7 +32,7 @@ class KNNclassifier:
         return np.array(y_predict)
 
     def _predict(self, x):
-        '''给定单个预测数据x，返回x的预测结果值'''
+        """给定单个预测数据x，返回x的预测结果值"""
         assert x.shape[0] == self._X_train.shape[1], \
             "the feature number of x must be equal to X_train"
 
@@ -42,6 +43,10 @@ class KNNclassifier:
         votes = Counter(topK_y)
 
         return votes.most_common(1)[0][0]
+
+    def score(self, X_test, y_test):
+        y_predict = self.predict(X_test)
+        return accuracy_score(y_test, y_predict)
 
     def __repr__(self):
         return "KNN(k=%d)" % self.k
